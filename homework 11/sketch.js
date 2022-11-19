0ff6f4e4141bb37563a02eb1a0936b00405830ef
grac-ies <106109485+grac-ies@users.noreply.github.com>
2022-11-19 16:02:02 -0700

    Add files via upload

---
+++ homework 11/sketch.js
@@ -0,0 +1,33 @@
+//all functions
+  //createColorPicker
+let colorPicker;
+//the character
+var guyX = 50;
+var guyY = 50;
+// your character moving
+var w = 25;
+var a = 25;
+var s = 25;
+var d = 25;
+//the killers
+var killer
+var killer
+var killer
+var killer
+{
+  createcanvas (600,800);
+  colorPicker = createColorPicker("pink");
+  // position color picker
+  colorPicker.position(225, 250);
+  // size color picker
+  colorPicker.size(50, 50);
+
+}
+function draw ()
+{
+  background(colorPicker.color());
+  stroke(0);
+  fill(0);
+  //the character
+  square(guyX,guyY,45)
+}
